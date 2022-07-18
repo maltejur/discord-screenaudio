46f9663c11c416fe59fd9c4c9a1b0c73403de3ea
Malte Jürgens <maltejur@dismail.de>
2022-07-18 20:29:57 +0200

    bump version to v1.0.0-rc.3

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
   QApplication::setApplicationName("discord-screenaudio");
-  QApplication::setApplicationVersion("1.0.0-rc.2");
+  QApplication::setApplicationVersion("1.0.0-rc.3");
 
   QCommandLineParser parser;
   parser.setApplicationDescription(
