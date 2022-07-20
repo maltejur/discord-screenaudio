b30c5cb43c0d67c4a6408f0f233ba9e7dd7da4d2
Malte Jürgens <maltejur@dismail.de>
2022-07-20 19:06:02 +0200

    lock rohrkabel to fixed version

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
   QApplication::setApplicationName("discord-screenaudio");
-  QApplication::setApplicationVersion("1.0.0-rc.4");
+  QApplication::setApplicationVersion("1.0.0-rc.5");
 
   QCommandLineParser parser;
   parser.setApplicationDescription(
