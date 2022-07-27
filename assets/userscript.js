6ef16b9625790a42ad62f3dede5f9c6cb96a8864
Malte Jürgens <maltejur@dismail.de>
2022-07-27 21:24:31 +0200

    bump version to 1.0.0-rc.7

--- assets/userscript.js
+++ assets/userscript.js
@@ -117,7 +117,7 @@ setInterval(() => {
   ) {
     for (const el of document.getElementsByClassName("info-3pQQBb")) {
       const aboutEl = document.createElement("div");
-      aboutEl.innerText = "discord-screenaudio v1.0.0-rc.6";
+      aboutEl.innerText = "discord-screenaudio v1.0.0-rc.7";
       aboutEl.style.fontSize = "12px";
       aboutEl.style.color = "var(--text-muted)";
       aboutEl.classList.add("dirscordScreenaudioAboutText");
