1a2d09fe3d965513f743b923166cd70d875077a7
Malte Jürgens <maltejur@web.de>
2022-08-02 11:46:37 +0200

    remove useless main.h again

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include "main.h"
-
 #include "mainwindow.h"
 #include "virtmic.h"
 
--- src/main.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <QString>
-
-int main(int argc, char *argv[]);
-void addToEnvVar(const char *var, const char *val);
